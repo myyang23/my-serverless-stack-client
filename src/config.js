431ec84168b7277a2ac35a1959debc25b8ff54e1
Mingyang Yang <Mingyang.Yang@ey.com>
2020-10-12 11:46:23 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "py-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://o9fdpj8et8.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_H9PfsdoTW",
+    APP_CLIENT_ID: "6drba5rgl88q482j1si7nse2tn",
+    IDENTITY_POOL_ID: "us-east-1:5616af36-c4c7-4a0d-bab5-3b711fccd1ec"
+  }
+};
